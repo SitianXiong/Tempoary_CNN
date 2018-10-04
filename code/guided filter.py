fa5cdd15aac13bed373fd8f32e18b1cfc42716d3
Boka Luo <42148420+BkLuo@users.noreply.github.com>
2018-10-04 14:12:02 -0400

    Add files via upload
    The code was rewritten into python based on the matlab code provided by Kaiming He . Reference: He, K., Sun, J., & Tang, X. (2010). Guided Image Filtering. Computer Vision – ECCV 2010. Springer Berlin Heidelberg.

---
+++ code/guided filter.py
@@ -0,0 +1,70 @@
+import gdal
+import numpy as np
+import cv2
+
+# Import files
+P_rf = gdal.Open(r'D:\Pass\Boka\MLP_test\11_62\pond_CNN\PanMrgAtCor_LC08_L1TP_011062_20150125_20170413_01_T1_B4.rst')  # input image
+I_rf = gdal.Open(r'D:\Pass\Boka\MLP_test\11_62\pond_CNN\PanMrgAtCor_LC08_L1TP_011062_20150125_20170413_01_T1_B4.rst')  # guide image
+
+
+# Parameters of the scene
+pj = P_rf.GetProjection()
+gt = P_rf.GetGeoTransform()
+col = P_rf.GetRasterBand(1).XSize
+row = P_rf.GetRasterBand(1).YSize
+
+# Read input as array
+P = P_rf.ReadAsArray()
+I = I_rf.ReadAsArray()
+
+
+# box filter
+def boxfilter(I, r):
+    bf = cv2.boxFilter(I, -1, (r, r))
+    return bf
+
+
+# Guided filter parameter setting
+# r: filter radius, eps: regularization coefficient
+for r in range(5, 32, 4):
+    for e in range(50, 400, 100):
+            eps = e ** 2
+
+            # Guided filter
+            def guidefilter(I, P, r, eps):
+                N = boxfilter(np.ones(np.shape(I)), r)
+
+                mean_I = boxfilter(I, r) / N
+                mean_P = boxfilter(P, r) / N
+                mean_IP = boxfilter(I * P, r) / N
+                cov_IP = mean_IP - mean_I * mean_P  # covariance of (I,P) in each local patch
+
+                mean_II = boxfilter(I * I, r) / N
+                var_I = mean_II - mean_I * mean_I
+
+                a = cov_IP / (var_I + eps)  # equation 5 in the paper
+                b = mean_P - a * mean_I  # equation 6 in the paper
+
+                mean_a = boxfilter(a, r) / N
+                mean_b = boxfilter(b, r) / N
+
+                q = mean_a * I + mean_b  # equation 8 in the paper
+                return q
+
+
+            output = guidefilter(I, P, r, eps)
+            out_path = r"D:\Pass\Boka\MLP_test\11_62\test\guided filter"
+            out_name = "\GF_11865_r" + str(r) + 'eps'+str(e)+'_B4_float32.rst'
+
+            # Write the result into an rst file
+            rst_driver = gdal.GetDriverByName('RST')
+            out_ds = rst_driver.Create(out_path + out_name, col, row, 1, gdal.GDT_Float32)
+            out_ds.GetRasterBand(1).WriteArray(output)
+            out_ds.SetProjection(pj)
+            out_ds.SetGeoTransform(gt)
+            del out_ds
+
+
+
+
+
